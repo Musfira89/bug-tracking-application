7f35808495312e9e4adc512cd695a85ea97876e4
Musfira <musfiraansari05@gmail.com>
2023-11-09 01:02:06 +0500

    add comments

--- app/api/Bug/route.tsx
+++ app/api/Bug/route.tsx
@@ -1,5 +1,6 @@
+
 import { NextRequest, NextResponse } from "next/server";
-import {z} from 'zod';  //for data validation
+import {z} from 'zod';  //for data validation check if client sent invalid data
 import prisma from "@/prisma/client";
 
 const createBugSchema = z.object({
@@ -10,12 +11,16 @@ const createBugSchema = z.object({
 export async function POST (request: NextRequest){
    const body = await request.json();
    const validation = createBugSchema.safeParse(body);
+
+   // validation start here's
    if (!validation.success){
     return NextResponse.json(validation.error.errors, {status: 400});}
+
+    //400 code means client sent invalid data
 
    const NewBug =   await prisma.debug.create({
     data: {title: body.title, description: body.description}
    });
-   return NextResponse.json(NewBug, {status: 201})   // 201 code means object
+   return NextResponse.json(NewBug, {status: 201})   // 201 code means object created (issue created) and stored in table
 
 }
